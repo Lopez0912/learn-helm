a2f264363b6a6434229399c3e28dd3564cea1172
Lopez0912 <114791592+Lopez0912@users.noreply.github.com>
2023-03-28 08:40:50 -0400

    app changes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Bienvenid@s al primer node app');
+  res.end('Bienvenid@s');
 });
 
 server.listen(port, hostname, () => {
